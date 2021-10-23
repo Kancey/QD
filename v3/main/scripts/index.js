476873753de82fee6cccd6b49ba8c40a24bc0929
Kancey <90845577+Kancey@users.noreply.github.com>
2021-10-23 19:13:23 +0600

    Delete v3 directory

--- v3/main/scripts/index.js
+++
@@ -1,8 +0,0 @@
-let menubtn = document.querySelector('.burger');
-let menu = document.querySelector('.menu-hidden');
-
-menubtn.addEventListener('click' , function(){
-  menubtn.classList.toggle('active');
-  menu.classList.toggle('active');
-})
-
