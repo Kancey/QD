3cef6a9ae6ecd0be1a9abfc928b79e5058c16677
Kancey <90845577+Kancey@users.noreply.github.com>
2021-10-31 21:34:58 +0600

    Почти самая лучшая версия
    Не смог разобраться со слайдером, так-же не доделал меню языков в мобильном варианте. Кстати, я сделал так, что-бы мобильная менюшка закрывалась когда нажимаешь на спан, который находится над меню.  Еще не смог сделать так, что-бы когда открываешь менюшку в мобильной версии, что-бы не скролилось вниз.

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,17 @@ let burgerm = document.querySelector('.burger-m');
 let burmenu = document.querySelector('.menu-m');
 let closem = document.querySelector('.up-thing');
 let body = document.querySelector('body');
-
+let openeye = document.querySelector('.open-eyes');
+let casesbar = document.querySelector('.cases-bar');
+let langup = document.querySelector('.language');
+let chglang = document.querySelector('.change-lng');
+let spanlng = document.querySelector('.change-lng-span');
+let spanlng1 = document.querySelector('.change-lng-span1');
+let openmenu = document.querySelector('.open-lng');
+let done1 = document.querySelector('.done1');
+let done2 = document.querySelector('.done2');
+let open1 = document.querySelector('.open1');
+let open2 = document.querySelector('.open2');
 
 
 
@@ -31,4 +41,24 @@ burgerm.addEventListener('click' , function(){
 
 closem.addEventListener('click' , function(){
   burmenu.classList.toggle('close');
+})
+
+openeye.addEventListener('click' , function(){
+  casesbar.classList.toggle('active');
+})
+
+langup.addEventListener('click' , function(){
+  chglang.classList.toggle('active');
+  spanlng.classList.toggle('active');
+  spanlng1.classList.toggle('active');
+  openmenu.classList.toggle('active');
+})
+
+
+open1.addEventListener('click' , function(){
+  done1.classList.toggle ('active');
+})
+
+open2.addEventListener('click' , function(){
+  done2.classList.toggle ('active');
 })
